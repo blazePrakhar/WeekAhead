3824fa2325ec6faf9f9f99ff5beec18ecb12f139
Tanmay0405 <tanmayawasthi070@gmail.com>
2026-09-15 22:45:14 +0530

    test: isolate health controller from security filters

--- backend/src/test/java/com/weekahead/health/HealthControllerTest.java
+++ backend/src/test/java/com/weekahead/health/HealthControllerTest.java
@@ -2,14 +2,19 @@ package com.weekahead.health;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.weekahead.config.SecurityConfig;
+
 @WebMvcTest(HealthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@Import(SecurityConfig.class)
 class HealthControllerTest {
 
     @Autowired
