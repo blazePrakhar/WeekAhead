7af11a51e3df78451bd96fabf90b9d16decc6fdd
Prakhar Raj Singh <prakharrajsingh10@gmail.com>
2026-09-27 02:10:45 +0530

    feat(frontend): implement dashboard UI

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import { getHealth } from "../api/healthApi";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import LifeAreasPage from "../pages/LifeAreasPage";
+import DashboardPage from "../pages/DashboardPage";
 import ProtectedRoute from "./ProtectedRoute";
 import ProtectedLayout from "../layouts/ProtectedLayout";
 import WeeklyAvailabilityPage from "../pages/WeeklyAvailabilityPage";
@@ -68,6 +69,7 @@ function AppRoutes() {
 
       <Route element={<ProtectedRoute />}>
         <Route element={<ProtectedLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/life-areas" element={<LifeAreasPage />} />
           <Route
             path="/weekly-availability"
@@ -75,7 +77,6 @@ function AppRoutes() {
           />
           <Route path="/time-allocation" element={<TimeAllocationPage />} />
           <Route path="/time-logs" element={<TimeLogsPage />} />
-          <Route path="/dashboard" element={<InsightsPage />} />
           <Route path="/insights" element={<InsightsPage />} />
           <Route path="/rebalancing" element={<RebalancingPage />} />
         </Route>
